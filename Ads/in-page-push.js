3fc7950c00f079e85eeb5d5876845ed1e8242d9f
aneeplayer <aneeplayer@gmail.com>
2025-07-29 09:31:20 +0700

    Create in-page-push.js

---
+++ Ads/in-page-push.js
@@ -0,0 +1,10 @@
+(function(dlwh){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = dlwh || {};
+s.src = "\/\/fondstudy.com\/b.XcVSsPdjGKlh0MYBW\/cl\/Oe\/mz9iurZ\/UOl\/klPLTtY\/1\/NYD\/IgzrNWTNcQtJNVj_Ub0kMhjXMI1ZOdAp";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
