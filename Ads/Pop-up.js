eab17a390fa96507deb239a065994d24fc78a271
aneeplayer <aneeplayer@gmail.com>
2025-07-29 15:04:14 +0700

    Create Pop-up.js

---
+++ Ads/Pop-up.js
@@ -0,0 +1,10 @@
+(function(fnkg){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = fnkg || {};
+s.src = "\/\/lazy-hurry.com\/ciD\/9J6Wb.2I5Bl\/SbW-QZ9KNkjyUF0XMSj\/kMzKNmyH0Q2UNvTnQhyCO\/TRMz4b";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
